9fae64a26f7cb97e380816bff657a6ed766fd14f
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-07 23:29:30 +0000

    code documented!

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Lockboxes"""
+
 
 def canUnlockAll(boxes):
+    """
+    unlocks the lockboxes and checks if they have all been unlocked or not
+    """
+
     if type(boxes) is not list or len(boxes) == 0:
         return False
 
