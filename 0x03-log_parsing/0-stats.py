15140ffffd84422e9e7d0aa582702dbb96d3bb75
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-21 13:22:02 +0000

    Debugged 0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == '__main__':
     file_size = [0]
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-            403: 0, 404: 0, 405: 0, 500: 0}
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
     def print_stats():
         """Print log"""
