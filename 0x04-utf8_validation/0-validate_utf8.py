cb896de7889e0ee144d77d1e8ee01e6d360c2f0f
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2024-01-04 20:21:08 +0000

    Debugging 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ def validUTF8(data):
     """Checks for UTF-8 validity in dataset"""
     for byte in data:
         bin_char = format(byte, '08b')
-        
+
         if bin_char.startswith('0'):
             continue
         elif bin_char.startswith('110'):
