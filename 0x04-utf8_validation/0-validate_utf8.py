47e7f354e4aac66f9f531b0ab091d7e9a7aef652
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2024-01-04 20:18:16 +0000

    Completed 0x04-utf8_validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a method that checks if an integer is UTF-8 valid"""
+
+
+def validUTF8(data):
+    """Checks for UTF-8 validity in dataset"""
+    for byte in data:
+        bin_char = format(byte, '08b')
+        
+        if bin_char.startswith('0'):
+            continue
+        elif bin_char.startswith('110'):
+            continue
+        elif bin_char.startswith('1110'):
+            continue
+        elif bin_char.startswith('11110'):
+            continue
+        else:
+            return False
+    return True
