3fadaea7c9673f913435bd0a42fe5099d2f20363
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2024-01-05 05:20:56 +0000

    Debugging 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,24 +5,17 @@
 def validUTF8(data):
     """Checks for UTF-8 validity in dataset"""
 
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-    
-    for num in data:
-        mask = 1 << 7
-        if num_bytes == 0:
-            while mask & num:
-                num_bytes += 1
-                mask = mask >> 1
+    for byte in data:
+        bin_char = format(byte, '08b')
 
-                if num_bytes == 0:
-                    continue
-
-                
-                if num_bytes == 1 or num_bytes > 4:
-                    return False
+        if bin_char.startswith('0'):
+            continue
+        elif bin_char.startswith('110'):
+            continue
+        elif bin_char.startswith('1110'):
+            continue
+        elif bin_char.startswith('11110'):
+            continue
         else:
-            if not (num & mask_1 and not (num & mask_2)):
-                return False
-        num_bytes -= 1
-    return num_bytes == 0
+            return False
+    return True
