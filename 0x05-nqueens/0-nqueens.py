ff89ce4e9905671bed29b6461e2612219a479068
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2024-01-11 12:27:51 +0000

    Debugging 0x05-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -7,15 +7,15 @@ try:
     N = int(sys.argv[1])
 except ValueError:
     print('N must be a number')
-    sys.exit(1)
+    exit(1)
 
 if len(sys.argv) != 2:
     print('Usage: nqueens N')
-    sys.exit(1)
+    exit(1)
 
 if N < 4:
     print('N must be at least 4')
-    sys.exit(1)
+    exit(1)
 
 
 board = []
