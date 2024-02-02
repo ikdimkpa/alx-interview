6439af773f72e86e3b3b5a461e9f32934fb70b5a
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2024-02-02 19:35:33 +0000

    Debugged 0x08-making_changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -24,5 +24,5 @@ def makeChange(coins, total):
 
     return -1
 
-#print(makeChange([1, 2, 25], 37))
-#print(makeChange([1256, 54, 48, 16, 102], 1453))
+# print(makeChange([1, 2, 25], 37))
+# print(makeChange([1256, 54, 48, 16, 102], 1453))
